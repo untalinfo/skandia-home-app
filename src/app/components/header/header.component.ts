c0fbf4b975656f84f515e646f56f2b4af1d0bb1a
Oscar Info <racso1607@gmail.com>
2024-05-03 09:48:49 -0500

    add sidebar with its items and its functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { HamburgerMenuComponent } from './hamburger-menu/hamburger-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ HamburgerMenuComponent ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Output() toggleSidebar = new EventEmitter<void>();
 
+  handleToggleSidebar(): void {
+    this.toggleSidebar.emit();
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { MainSectionComponent } from '../../components/main-section/main-section.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ HeaderComponent, MainSectionComponent, FooterComponent ],
+  imports: [ HeaderComponent, MainSectionComponent, FooterComponent, SidebarComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 
 export class HomeComponent {
+  isSidebarExpanded: boolean = false;
 
+  toggleSidebar(): void {
+    this.isSidebarExpanded = !this.isSidebarExpanded;
+  }
 }
